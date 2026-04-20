17fdc4e43583697d373afe171e02e9688454f2bc
Archie Payne <84518986+sn0wyGhost@users.noreply.github.com>
2026-04-20 12:30:25 +0100

    Add files via upload

---
+++ _main.py
@@ -0,0 +1,73 @@
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+
+import pandas as pd
+import json
+
+### Define dependencies ###
+BASE_DIR = Path(__file__).resolve().parent.parent
+ENV_PATH = BASE_DIR / ".env"
+load_dotenv(ENV_PATH)
+
+cemar_public_key = os.getenv("cemar_public_key")
+cemar_private_key = os.getenv("cemar_private_key")
+
+if not cemar_public_key:
+    raise RuntimeError("cemar_public_key is not set")
+if not cemar_private_key:
+    raise RuntimeError("cemar_private_key is not set")
+
+### App ###
+from cemar_sdk import sdk
+client = sdk()
+client.InitialiseSession(cemar_public_key, cemar_private_key)
+
+pageNumber = 0 
+pageSize =  100000
+
+# Get contracts list
+contracts_json = client.GetContracts(pageNumber, pageSize)
+
+# Create contracts data frame
+contracts_df = pd.DataFrame(contracts_json["data"])
+# contracts_df.to_csv("contracts.csv", index=False, encoding="utf-8")
+
+# Get task orders data
+task_orders_data = []
+for row in contracts_df.itertuples(index=False):
+    contract_id = row.contractId
+    task_orders_json = client.GetTaskOrders(contract_id, pageNumber, pageSize)
+    try:
+        for item in task_orders_json["data"]:
+            item["contractId"] = contract_id
+        task_orders_data.extend(task_orders_json["data"]) 
+    except:
+        print(f"Contract {contract_id} task orders contained no data")
+
+# Create task orders data frame
+task_orders_df = pd.DataFrame(task_orders_data)
+# task_orders_df.to_csv("task_orders.csv", index=False, encoding="utf-8")
+
+# Get compensation events data
+compensation_events_data = []
+for row in contracts_df.itertuples(index=False):
+    contract_id = row.contractId
+    compensation_events_json = client.GetCompensationEvents(contract_id, pageNumber, pageSize)
+    try:
+        for item in compensation_events_json["data"]:
+            item["contractId"] = contract_id
+        compensation_events_data.extend(compensation_events_json["data"]) 
+    except:
+        print(f"Contract {contract_id} compensation events contained no data")
+
+# Create task orders data frame
+compensation_events_df = pd.DataFrame(compensation_events_data)
+# compensation_events_df.to_csv("compensation_events.csv", index=False, encoding="utf-8")
+
+# Upload data to pre-established Smartsheet sheets
+from smartsheet_handler import handler
+sh = handler()
+sh.UpdateSheet(contracts_df, sh.contracts_sheet_id)
+sh.UpdateSheet(task_orders_df, sh.task_orders_sheet_id)
+sh.UpdateSheet(compensation_events_df, sh.compensation_events_sheet_id)
---
+++ smartsheet_handler.py
@@ -0,0 +1,105 @@
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+
+import numpy as np
+import pandas as pd
+import smartsheet
+from datetime import datetime
+
+class handler:
+
+    ### Define dependencies ###
+    BASE_DIR = Path(__file__).resolve().parent.parent
+    ENV_PATH = BASE_DIR / ".env"
+    load_dotenv(ENV_PATH)
+
+    smartsheet_access_token = os.getenv("smartsheet_access_token")
+
+    if not smartsheet_access_token:
+        raise RuntimeError("smartsheet_access_token is not set")
+
+    contracts_sheet_id = os.getenv("contracts_sheet_id")
+    task_orders_sheet_id = os.getenv("task_orders_sheet_id")
+    compensation_events_sheet_id = os.getenv("compensation_events_sheet_id")
+
+    ### App ###
+    smart = None
+
+    def __init__(self):
+        self.smart = smartsheet.Smartsheet(access_token=self.smartsheet_access_token, api_base="https://api.smartsheet.eu/2.0/")
+
+    def  UpdateSheet(self, dataframe, sheet_id):
+        # Get sheet
+        sheet = self.smart.Sheets.get_sheet(sheet_id)
+
+        # Extract column names from dataframe into an array
+        columns = dataframe.columns.tolist()
+
+        # Create an array of Smartsheet sheet's column IDs in same order as dataframe
+        column_ids = []
+        for column in columns:
+            id = sheet.get_column_by_title(column).id
+            column_ids.append(id)
+
+        # Clean dataframe
+        # cleaned_dataframe = clean_for_json(dataframe)
+        
+        # Create new rows for upload, disregard cells containing arrays
+        upload_rows = []
+        for row in dataframe.itertuples(index=False):
+            new_row = smartsheet.models.Row()
+            new_row.to_bottom = True
+            for col_id, value in zip(column_ids, row):
+                if value != None:
+                    if isinstance(value, (list, tuple)):
+                        continue
+                    else:
+                        string = str(value) # Clean dataframe isn't working, intermediate fix
+                        try:
+                            dt = datetime.strptime(string, "%Y-%m-%dT%H:%M:%S")
+                            string = dt.strftime("%d/%m/%Y")
+                        except:
+                            None
+                        new_row.cells.append({
+                            'column_id': col_id,
+                            'value': string, 
+                            'strict': False
+                        })
+            upload_rows.append(new_row)
+
+        # Upload new rows to sheet
+        batch_size = 250
+
+        upload_rows_count = len(upload_rows)
+        for start in range(0, upload_rows_count, batch_size):
+            end = min(start + batch_size, upload_rows_count)
+            batch = upload_rows[start:end]
+            self.smart.Sheets.add_rows(sheet_id, batch)        
+
+        # Remove old rows from sheet
+        delete_rows_ids = [row.id for row in sheet.rows]
+        delete_rows_ids_count = len(delete_rows_ids)
+        for start in range(0, delete_rows_ids_count, batch_size):
+            end = min(start + batch_size, delete_rows_ids_count)
+            batch = delete_rows_ids[start:end]
+            self.smart.Sheets.delete_rows(sheet_id, batch, True)
+
+        print(f"{sheet.name} sheet successfully updated")
+
+    
+def clean_for_json(df: pd.DataFrame) -> pd.DataFrame:
+    # Replace NaN/NaT/inf/-inf with None
+    cleaned = df.copy()
+
+    # Replace inf and -inf (they’re not JSON-compliant either)
+    cleaned = cleaned.replace([np.inf, -np.inf], np.nan)
+
+    # Convert datetimes; NaT -> None; valid datetimes -> ISO8601 string
+    for col in cleaned.select_dtypes(include=["datetime64[ns]", "datetime64[ns, UTC]"]).columns:
+        cleaned[col] = cleaned[col].apply(lambda x: x.isoformat() if pd.notna(x) else None)
+
+    # Convert remaining NaN to None
+    cleaned = cleaned.where(pd.notnull(cleaned), None)
+
+    return cleaned
